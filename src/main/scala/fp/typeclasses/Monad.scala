e8f2fc8763a17a459a4916bf440d3860023ced37
Kleber Stender <kleber.stender@gmail.com>
2014-08-08 07:06:00 -0300

    Instead of using Traversable and Iterable traits using Monad and Functor

--- src/main/scala/fp/typeclasses/Monad.scala
+++ src/main/scala/fp/typeclasses/Monad.scala
@@ -2,4 +2,9 @@ package fp.typeclasses
 
 trait Monad[A] extends Functor[A]{
   def flatMap[B]: (A => Monad[B]) => Monad[B]
+  
+  def foreach[U]: (A => U) => Unit = (fn) => {
+    map(fn)
+    ()
+  }
 }
